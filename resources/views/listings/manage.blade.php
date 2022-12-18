89760159bc4a0089771435c3e3c304f0fa011496
AbbasKagdi <abkagdibas@gmail.com>
2022-12-18 22:57:53 +0300

    edit + delete links on manage listings

--- resources/views/listings/manage.blade.php
+++ resources/views/listings/manage.blade.php
@@ -2,14 +2,26 @@
     <div class="container my-5">
         <p class="display-4">Manage your listings</p>
         <table class="table table-hover display-6">
-            @foreach($listings as $listing)
-                <tr>
-                    <td><a href="/listings/{{$listing->id}}"><img src="{{$listing->logo ? asset('/storage/'. $listing->logo) : asset('/images/laravel.png')}}" class="img-fluid" width="100px" alt="{{$listing->company}} logo"></a></td>
-                    <td>{{$listing->title}}</td>
-                    <td><a class="td-0" href="/listings/{{$listing->id}}/edit">Edit</a></td>
-                    <td><a class="td-0" href="/listings/{{$listing->id}}/delete">Delete</a></td>
-                </tr>
-            @endforeach
+            @unless($listings->isEmpty())
+                @foreach($listings as $listing)
+                    <tr>
+                        <td><a href="/listings/{{$listing->id}}"><img src="{{$listing->logo ? asset('/storage/'. $listing->logo) : asset('/images/laravel.png')}}" class="img-fluid" width="100px" alt="{{$listing->company}} logo"></a></td>
+                        <td>{{$listing->title}}</td>
+                        <td><a class="td-0 text-bold" href="/listings/{{$listing->id}}/edit">Edit <i class="fas fa-pencil-alt"></i></a></td>
+                        <td>
+                            <form method="POST" action="/listings/{{$listing->id}}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="border-0 text-danger bg-transparent td-0">
+                                    Delete <i class="fas fa-trash"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            @else()
+                <tr><td>No listings to show</td></tr>
+            @endunless
         </table>
     </div>
 </x-layout>
