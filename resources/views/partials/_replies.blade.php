9df20060039f7ca6b36ee1564980ea8ceb96d5a7
AbbasKagdi <abkagdibas@gmail.com>
2022-12-20 19:37:25 +0300

    unstable comments more replies

--- resources/views/partials/_replies.blade.php
+++ resources/views/partials/_replies.blade.php
@@ -22,7 +22,20 @@
                                     <div class="bg-light p-1 my-2 border-danger border-start">
                                         <div class="ms-2">
                                             <b>>>>{{$reply->user->name}}</b>
-                                            <p>>>>{{$reply->comment}}</p>                                                                
+                                            <p>>>>{{$reply->comment}}</p>
+                                            
+                                            @if (count($reply->replies) > 0)
+                                                @foreach ($reply->replies as $reply)
+                                                    {{-- {{dd($reply)}} --}}
+                                                    <div class="bg-light p-1 my-2 border-info border-start">
+                                                        <div class="ms-2">
+                                                            <b>>>>>{{$reply->user->name}}</b>
+                                                            <p>>>>>{{$reply->comment}}</p>                                                                
+                                                        </div>
+                                                    </div>
+                                                @endforeach
+                                            @endif
+                                            
                                         </div>
                                     </div>
                                 @endforeach
